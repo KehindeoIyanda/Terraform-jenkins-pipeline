58cf9123b94baa088eabe3df76b99a364b236787
kehindeoiyanda <kehindeola96@yahoo.com>
2025-04-13 20:27:46 -0400

    updated Jenkinsfile

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+  
+}
+
+
+variable "ami" {
+   type        = string
+   description = "Ubuntu AMI ID"
+   default     = "ami-04f167a56786e4b09"
+
+}
+
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t2.micro"
+}
+
+variable "name_tag" {
+   type        = string
+   description = "Name of the EC2 instance"
+   default     = "EC2 instance"
+}
